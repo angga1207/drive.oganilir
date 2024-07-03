88309975eeae84ae2420cd51055a604ed6a9214b
angga1207 <63151469+angga1207@users.noreply.github.com>
2024-07-03 10:05:42 +0700

    update endpoint google auth

--- pages/login.tsx
+++ pages/login.tsx
@@ -331,7 +331,7 @@ const Login = () => {
                                             <button
                                                 disabled={isLoading}
                                                 type='submit'
-                                                className="w-full bg-slate-100 text-black hover:text-white rounded px-3 py-2 focus:outline-none transition-all duration-300 hover:bg-green-500 flex items-center justify-center cursor-pointer">
+                                                className="w-full bg-slate-100 text-black hover:text-white rounded px-3 py-4 focus:outline-none transition-all duration-300 hover:bg-green-500 flex items-center justify-center cursor-pointer">
                                                 <FontAwesomeIcon icon={faSignInAlt} className="w-4 h-4 mr-2" />
                                                 <div className="font-bold tracking-widest">
                                                     MASUK
@@ -339,7 +339,7 @@ const Login = () => {
                                             </button>
                                         </div>
                                         <hr />
-                                        <div className="">
+                                        <div className="opacity-0">
                                             <div className="text-center text-white">
                                                 Masuk Menggunakan :
                                             </div>
